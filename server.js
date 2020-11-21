2eb5ff11bcf18d02c2137b345ad8acc49a0dc524
Sean Farley <somethingfarley@gmail.com>
2020-11-21 11:55:44 -0500

    Week 1 workshop part 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
-
+const partnerRouter = require('./routes/partnerRouter')
 
 
 const hostname = 'localhost';
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
-
+app.use('/partners', partnerRouter)
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) =>{
